4447d55191d9a67b4f1957f65c04b20c39aa1c09
davlatabdullaev <davlatdavlat0206@gmail.com>
2023-11-18 04:10:21 +0500

    Uacademy lesson 4 class works

---
+++ lesson4.go
@@ -0,0 +1,70 @@
+package main
+
+func main() {
+  // 1-masala
+  /*  var a int = 5
+    var b int = 10
+
+    a = a + b
+    b = a - b
+    a = a - b
+    fmt.Println("a = ", a)
+    fmt.Println("b = ", b) */
+
+  // 4-masala
+  /*
+    var (
+      a int
+      b int
+      c int
+    )
+    fmt.Println(" a ni kiriting ")
+    fmt.Scan(&a)
+    fmt.Println(" a ni kiriting ")
+    fmt.Scan(&b)
+    fmt.Println(" a ni kiriting ")
+    fmt.Scan(&c)
+
+    var shart bool = ((a+b > c) && (a+c > b) && (b+c > a))
+
+    fmt.Println(shart)
+  */
+
+  // 5-masala
+
+  /*  var a int
+    fmt.Println(" a = ")
+    fmt.Scan(&a)
+    fmt.Println("Berilgan kvadratni yuzi ", (a * a))
+  */
+
+  // 6-masala
+  /*  var d float32
+    fmt.Println(" diametrini kiriting = ")
+    fmt.Scan(&d)
+    const pi float32 = 3.14
+    fmt.Println("Aylana uzunligi ", (d * pi)) */
+
+  // 7-masala
+  /*    var a int
+  fmt.Println(" a = ")
+  fmt.Scan(&a)
+  fmt.Println("Sonni kvadrati ", (a * a)) */
+
+  // 8-masala
+  /*  var a int
+    fmt.Println(" a = ")
+    fmt.Scan(&a)
+    var b int
+    fmt.Println(" b = ")
+    fmt.Scan(&b)
+
+    fmt.Println("Sonni kvadrati ", (2 * (a + b))) */
+
+  // 9-masala
+  /*  var a int
+    fmt.Println(" a = ")
+    fmt.Scan(&a)
+    fmt.Println("Berilgan kvadratni perimetri ", (4*a)) */
+
+}
